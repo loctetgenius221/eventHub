06b70f6a13a919b8af8b81b56c5d9927a701666e
loctetgenius <sagnamoussa546@gmail.com>
2024-06-27 09:42:16 +0000

    Test pour voir si les dossiers creer dans le view sont sur github

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AssociationController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 // Route::resource('associations', AssociationController::class)->middleware('role:association');
 
